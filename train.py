43c12dc35f1a416311c58221971883a8fc448e82
Dat Ngo <quocdat32461997@yahoo.com>
2026-03-06 20:48:37 -0600

    Updated data pipeline: 1) added amazon-meta-dataset

--- train.py
+++ train.py
@@ -1,1 +1,29 @@
-import trl
+import argparse
+
+import yaml
+from datasets import load_dataset
+
+from trainer import QuanSFTTrainer
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--config-path", type=str)
+args = parser.parse_args()
+
+
+def main():
+    with open(args.config_path, "r") as f:
+        config = yaml.safe_load(f)
+
+    meta_data = load_dataset(
+        "McAuley-Lab/Amazon-Reviews-2023",
+        f"raw_meta_{config['category']}",
+        split="full",
+        trust_remote_code=True,
+    )
+
+    trainer = QuanSFTTrainer()
+    trainer.train(meta_data)
+
+
+if __name__ == "__main__":
+    main()
