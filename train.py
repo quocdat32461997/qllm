0df6499f9a7f82bf13c200767ef8ef129032bdd3
Dat Ngo <quocdat32461997@yahoo.com>
2026-03-06 20:51:08 -0600

    Updated training pipeline

--- train.py
+++ train.py
@@ -1,19 +1,23 @@
 import argparse
 
+import mlflow
 import yaml
 from datasets import load_dataset
-
 from trainer import QuanSFTTrainer
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--config-path", type=str)
-args = parser.parse_args()
 
 
-def main():
+if __name__ == "__main__":
+    # Get command line arguments
+    args = parser.parse_args()
+
+    # Load configuration
     with open(args.config_path, "r") as f:
         config = yaml.safe_load(f)
 
+    # Load dataset
     meta_data = load_dataset(
         "McAuley-Lab/Amazon-Reviews-2023",
         f"raw_meta_{config['category']}",
@@ -21,9 +25,9 @@ def main():
         trust_remote_code=True,
     )
 
-    trainer = QuanSFTTrainer()
-    trainer.train(meta_data)
-
-
-if __name__ == "__main__":
-    main()
+    # Train
+    mlflow.set_experiment(config["experiment_name"])
+    mlflow.set_tracking_uri(config["mlflow_tracking_uri"])
+    with mlflow.start_run() as run:
+        trainer = QuanSFTTrainer()
+        trainer.train(meta_data)
