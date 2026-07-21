9c6699f74882c43c7e57b618b3f44a995aa699c3
Dat Ngo <quocdat32461997@yahoo.com>
2026-07-20 21:10:37 -0500

    Update

--- trainers.py
+++ trainers.py
@@ -108,7 +108,7 @@ class QuanSFTTrainer(Trainer):
         result = super().training_step(model, inputs, num_items_in_batch)
 
         # Log current learning rate
-        self.log({"learning_rate": self.optimizer.param_groups[0]["lr"]})
+        self.log({"lr": self.optimizer.param_groups[0]["lr"]})
 
         # Debug: Check LoRA gradients after backward pass
         # if self.step_count < 3:  # Only check first few steps
